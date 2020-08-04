3d1a22e910c57533d9aee4d4b639585b84ea3756
Alim Nastaev <a.nastaev@gmail.com>
2020-08-04 11:31:32 -0400

    final

--- hello_world/hello_world.exs
+++ hello_world/hello_world.exs
@@ -1,10 +1,25 @@
 defmodule HelloWorld do
   @englishHelloPrefix "Hello, "
 
-  def hello_world(name) do
-    if name == "",
+  @spanish "Spanish"
+  @spanishHelloPrefix "Hola, "
+
+  @french "French"
+  @frenchHelloPrefix "Bonjour, "
+
+  def hello_world(name \\ "", language \\ "") do
+    if name == "" && language == "",
       do: @englishHelloPrefix <> "World",
-      else: @englishHelloPrefix <> name
+      else: language_selection(language, name)
+  end
+
+  defp language_selection(language, name) do
+    case language do
+      "" -> @englishHelloPrefix <> name
+      @spanish -> @spanishHelloPrefix <> name
+      @french -> @frenchHelloPrefix <> name
+
+    end
   end
 end
 
@@ -15,11 +30,33 @@ defmodule HelloWorldTest do
 
   import HelloWorld
 
+  defp assert_correct_message(got, want) do
+    assert want == got
+  end
+
   test "succes case - saying hello to people" do
-    assert hello_world("Alim") == "Hello, Alim"
+    want = hello_world("Bob")
+    got = "Hello, Bob"
+
+    assert_correct_message(got, want)
   end
 
   test "say 'Hello, World' when an empty string is supplied" do
-    assert hello_world("") == "Hello, World"
+    got = hello_world()
+    want = "Hello, World"
+
+    assert_correct_message(got, want)
+  end
+
+  test "in Spanish" do
+    got = hello_world("Bob", "Spanish")
+    want = "Hola, Bob"
+    assert_correct_message(got, want)
+  end
+
+  test "in French" do
+    got = hello_world("Bob", "French")
+    want = "Bonjour, Bob"
+    assert_correct_message(got, want)
   end
 end
